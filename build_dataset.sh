7a1e868192a190bf7988368f3690f0ca04070819
Emmanuel <37215934+emmanuel-aubertin@users.noreply.github.com>
2024-12-29 16:41:37 +0100

    👍 Skip intro and outro 👍

--- build_dataset.sh
+++ build_dataset.sh
@@ -14,7 +14,8 @@ keep_mp4=0
 links_file="links.json"
 tmp_directory="tmp_video"
 output_directory="output"
-number_img=200
+number_img=100
+skip_download=0
 
 print_release() {
     echo "$RELEASE $AUTHOR"
@@ -34,9 +35,10 @@ print_usage() {
         echo "          -d  Use youtube-dl (by default)"
         echo "          -y  Use yt-dlp"
         echo "          -f  Input file with youtube links (see PUT LINK TO DOC LATER)" 
-        echo "          -n  Number of images extracted from each video (200 by default)"
+        echo "          -n  Number of images extracted from each video"
         echo "          -o  Specify the output directory (output/ by default)"
         echo "          -t  Specify the path to the video temps directory (tmp_video/ by default)"
+        echo "          -s  Skip video download"
         echo ""
         echo "Exemple: ./build_dataset.sh -o dataset/cheater -y -f apex_cheater.json -t tmp_cheater_vids -k"
         echo ""
@@ -84,6 +86,9 @@ while [ $# -gt 0 ]; do
             number_img=$2
             shift
             ;;
+        -s | --skip)
+            skip_download=1
+            ;;
         -V | --verbose)
             VERBOSE=1
             ;;
@@ -127,9 +132,9 @@ function ask_yes_or_no() {
     fi
     return 1
 }
-
 function dl_vids() {
     local video_url="$1"
+    local video_title
 
     if ! command -v $downloader &> /dev/null; then
         colored_echo "1;31m" "Error: $downloader is not installed. Please install it and try again."
@@ -141,14 +146,25 @@ function dl_vids() {
         mkdir -p "$tmp_directory"
     fi
 
+    video_title=$($downloader --get-filename -o "%(title)s.mp4" "$video_url" 2>/dev/null)
+    if [ -z "$video_title" ]; then
+        colored_echo "1;31m" "Error: Unable to fetch video title. Skipping..."
+        return
+    fi
+
+    if [ -f "$tmp_directory/$video_title" ]; then
+        colored_echo "1;33m" "Video already downloaded: $video_title. Skipping..."
+        return
+    fi
+
     log_verbose "Downloading video from $video_url to $tmp_directory using $downloader..."
-    $downloader -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4"  -o "$tmp_directory/%(title)s.mp4" "$video_url" &> /dev/null
+    $downloader -o "$tmp_directory/%(title)s.mp4" "$video_url" &> /dev/null
 
     if [ $? -eq 0 ]; then
-        colored_echo "1;32m" "Download completed successfully."
+        colored_echo "1;32m" "Download completed successfully: $video_title."
         log_verbose "Download details: Video URL - $video_url"
     else
-        colored_echo "1;31m" "Error: Download failed."
+        colored_echo "1;31m" "Error: Download failed for $video_url."
         exit 1
     fi
 }
@@ -195,12 +211,24 @@ function extract_images() {
             colored_echo "1;31m" "Error: Unable to determine frame count for $file."
             continue
         fi
+
+        # Ignore Intro and outro (10 seconds)
+        video_duration=$(ffprobe -v error -show_entries format=duration -of default=nokey=1:noprint_wrappers=1 "$file")
+        if [ -z "$video_duration" ] || (( $(echo "$video_duration < 30" | bc -l) )); then
+            colored_echo "1;31m" "Error: Video too short to process (less than 30 seconds). Skipping $file."
+            continue
+        fi
+
+        start_time=15
+        end_time=$(echo "$video_duration - 15" | bc)
+
         frame_interval=$((total_frames / number_img))
         if [ "$frame_interval" -eq 0 ]; then
             frame_interval=1
         fi
-        log_verbose "Extracting images from $file with interval $frame_interval..."
-        ffmpeg -i "$file" -vf "select=not(mod(n\,$frame_interval))" -vsync vfr "$output_directory/${filename}_%04d.png" &> /dev/null
+
+        log_verbose "Extracting images from $file with interval $frame_interval, ignoring first 15s and last 15s..."
+        ffmpeg -i "$file" -vf "select='between(t,$start_time,$end_time)*not(mod(n\,$frame_interval))'" -vsync vfr "$output_directory/${filename}_%04d.png" &> /dev/null
 
         if [ $? -eq 0 ]; then
             colored_echo "1;32m" "Images extracted successfully for $file."
@@ -210,8 +238,13 @@ function extract_images() {
     done
 }
 
-parse_links_file
-colored_echo "32m" "Download complete."
+
+if [ $skip_download -eq 0 ]; then
+    parse_links_file
+    colored_echo "32m" "Download complete."
+else
+    colored_echo "32m" "Skipping Download."
+fi
 
 extract_images
 
